4d8f61552dad545e10d717f1c762abd49dadf100
J.S.LI <jsli@linux.com>
2024-10-07 23:51:19 +0800

    feat: add entities for location, trip and strategy.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@
 
 source 'https://rubygems.org'
 
+# Production Dependency
 gem 'http'
+
+# Testing
+gem 'minitest'
--- lib/services/llama_api.rb
+++ lib/services/llama_api.rb
@@ -17,10 +17,8 @@ class LlamaAPI
   # @param prompt [String] The text prompt for the completion.
   # @param model  [String] The model to use (default: 'meta-llama/Llama-3.2-1B').
   def generate_text(prompt, model = 'meta-llama/Llama-3.2-1B')
-    response = @http.post("/models/#{model}",
-                          headers: { 'Authorization' => "Bearer #{@secret}" },
-                          json: { inputs: prompt })
-    output = JSON.parse(response.body)
-    output[0]['generated_text']
+    @http.post("/models/#{model}",
+               headers: { 'Authorization' => "Bearer #{@secret}" },
+               json: { inputs: prompt }).parse
   end
 end
