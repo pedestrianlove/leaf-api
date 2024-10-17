d5af9d898dc92e7d75abe15b2a953e4669d84d8b
harvey <sideman8787@gmail.com>
2024-10-17 20:23:50 +0800

    feat: add location_mapper & spec

--- lib/mappers/location_mapper.rb
+++ lib/mappers/location_mapper.rb
@@ -13,25 +13,59 @@ module LeafAPI
       # TODO: Refer to the TripMapper and Finish the LocationMapper class...
       def find(address)
         # FIXME: 為了先讓測試能過隨便給的，要改掉照著TripMapper的樣子寫!!!
-        LeafAPI::Entity::Location.new(
-          id: nil,
-          name: address,
-          latitude: 123.1,
-          longtitude: 456.1
-        )
+        # LeafAPI::Entity::Location.new(
+        #  id: nil,
+        #  name: address,
+        #  latitude: 123.1,
+        #  longtitude: 456.1
+        # )
+        data = @gateway.geocoding(address)
+        build_entity(data)
       end
 
+      def build_entity(data)
+        DataMapper.new(data, @token, @gateway_class).build_entity
+      end
+
+      # This class maps the response data from Google Maps API to a Location entity.
+      # It extracts necessary information such as name, latitude, and longitude.
+      class DataMapper
+        def initialize(data, token, gateway_class)
+          @data = data
+          @location_mapper = LocationMapper.new(
+            gateway_class, token
+          )
+        end
+
+        def build_entity
+          LeafAPI::Entity::Location.new(
+            id: nil,
+            name: name,
+            latitude: latitude,
+            longtitude: longtitude
+          )
+        end
+
+        def name
+          @data['result'][0]['formatted_address']
+        end
+
+        def latitude
+          @data['result'][0]['geometry']['location']['lat']
+        end
+
+        def longtitude
+          @data['result'][0]['geometry']['location']['lng']
+        end
+      end
       # TODO: Finish this function.
-      def build_entity(data); end
+      #def name; end
 
       # TODO: Finish this function.
-      def name; end
+      #def latitude; end
 
       # TODO: Finish this function.
-      def latitude; end
-
-      # TODO: Finish this function.
-      def longtitude; end
+      #def longtitude; end
     end
   end
 end
