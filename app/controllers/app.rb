f0456c73427dc1fa471f34a1ef18ffdf560de5c4
harvey <sideman8787@gmail.com>
2024-10-20 00:39:32 +0800

    feat: add views for location & add location for mvc Harvey

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,6 +2,10 @@
 
 require 'roda'
 require 'slim'
+require_relative '../models/mappers/location_mapper'
+require_relative '../models/gateways/google_maps_api'
+
+CORRECT_SECRETS = YAML.safe_load_file('config/secrets.yaml')
 
 module LeafAPI
   # Web App
@@ -22,9 +26,28 @@ module LeafAPI
 
       # Manage Location resources
       routing.on 'locations' do
+        # for search (POST /locations/search)
+        routing.post 'search' do
+          # puts "Received params: #{routing.params.inspect}" # 輸出所有請求參數
+          location_query = routing.params['location'].downcase
+          # puts "Received location query: #{location_query}" # 日誌輸出
+          routing.redirect "/locations/#{CGI.escape(location_query)}"
+        end
+
         routing.is do
+          # show seraching table (GET /locations)
           routing.get do
-            view 'location'
+            view 'location_form'
+          end
+        end
+
+        # show the searching results (GET /locations/[location_query])
+        routing.on String do |location_query|
+          routing.get do
+            # apply Mapper to get location info
+            location_entity = LeafAPI::GoogleMaps::LocationMapper.new(LeafAPI::GoogleMaps::API, CORRECT_SECRETS['GOOGLE_TOKEN']).find(location_query)
+            # show the searching results
+            view 'location_result', locals: { location: location_entity }
           end
         end
       end
