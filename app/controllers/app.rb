16b68474334264895c723bf0aa64b659c91b8e7b
harvey <sideman8787@gmail.com>
2024-10-21 00:26:04 +0800

    recover git blame record

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -35,50 +35,6 @@ module LeafAPI
       end
     end
 
-    def setup_location_routes(routing)
-      routing.on 'locations' do
-        setup_location_search(routing)
-        setup_location_form(routing)
-        setup_location_result(routing)
-      end
-    end
-    
-    def setup_location_search(routing)
-      routing.post 'search' do
-        handle_search(routing)
-      end
-    end
-
-    def setup_location_form(routing)
-      routing.is do
-        routing.get do
-          view 'location_form'
-        end
-      end
-    end
-
-    def setup_location_result(routing)
-      routing.on String do |location_query|
-        routing.get do
-          handle_location_query(location_query)
-        end
-      end
-    end
-
-    def handle_search(routing)
-      location_query = routing.params['location'].downcase
-      routing.redirect "/locations/#{CGI.escape(location_query)}"
-    end
-
-    def handle_location_query(location_query)
-      location_entity = LeafAPI::GoogleMaps::LocationMapper.new(
-        LeafAPI::GoogleMaps::API,
-        CONFIG['GOOGLE_TOKEN']
-      ).find(location_query)
-
-      view 'location_result', locals: { location: location_entity }
-    end
-
     def setup_trip_routes(routing)
       routing.on 'trips' do
         setup_trip_submit(routing)
